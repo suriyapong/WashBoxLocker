626e8273a5c4beda729c0c3ccea73d0a3ac8bbda
Suriyapong <suriyapong_s@hotmail.com>
2019-09-11 11:43:11 +0100

    Add Customservice

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -3,6 +3,7 @@ const record = require('./record/record.service.js');
 const checkAvailable = require('./check-available/check-available.service.js');
 const dropOff = require('./drop-off/drop-off.service.js');
 const cleanDropOff = require('./clean-drop-off/clean-drop-off.service.js');
+const customService = require('./custom-service/custom-service.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(locker);
@@ -10,4 +11,5 @@ module.exports = function (app) {
   app.configure(checkAvailable);
   app.configure(dropOff);
   app.configure(cleanDropOff);
+  app.configure(customService);
 };
