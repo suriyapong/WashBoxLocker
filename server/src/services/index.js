c32cb30db472a0951c6af89a9587fae36a815732
Suriyapong <suriyapong_s@hotmail.com>
2019-09-16 13:29:27 +0100

    ลบไฟล์ไม่ได้ใช้

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -3,7 +3,6 @@ const record = require('./record/record.service.js');
 const checkAvailable = require('./check-available/check-available.service.js');
 const dropOff = require('./drop-off/drop-off.service.js');
 const cleanDropOff = require('./clean-drop-off/clean-drop-off.service.js');
-const customService = require('./custom-service/custom-service.service.js');
 const log = require('./log/log.service.js');
 const user = require('./user/user.service.js');
 const role = require('./role/role.service.js');
@@ -16,7 +15,6 @@ module.exports = function (app) {
   app.configure(checkAvailable);
   app.configure(dropOff);
   app.configure(cleanDropOff);
-  app.configure(customService);
   app.configure(log);
   app.configure(user);
   app.configure(role);
